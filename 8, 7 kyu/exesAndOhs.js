11be5ae59bbe311ac1f43fc6e319e4de50402d05
Dok92 <doslovdok@gmail.com>
2022-08-08 16:40:26 +0200

    new katas

--- 8, 7 kyu/exesAndOhs.js
+++ 8, 7 kyu/exesAndOhs.js
@@ -1,4 +1,6 @@
-// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive.
+// The string can contain any char.
+
 //* 1
 function XO(str) {
   let regX = str.match(/x/gi)
